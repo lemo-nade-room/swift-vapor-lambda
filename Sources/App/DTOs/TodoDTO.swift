7071f084b65424267ac3ce7f7bdf542a2d1bd8b0
tanaka <lemo.nade.engineer.room@gmail.com>
2024-12-30 21:04:13 +0900

    Swift Format

--- Sources/App/DTOs/TodoDTO.swift
+++ Sources/App/DTOs/TodoDTO.swift
@@ -4,10 +4,10 @@ import Vapor
 struct TodoDTO: Content {
     var id: UUID?
     var title: String?
-    
+
     func toModel() -> Todo {
         let model = Todo()
-        
+
         model.id = self.id
         if let title = self.title {
             model.title = title
--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,4 +1,5 @@
 import Fluent
+
 import struct Foundation.UUID
 
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
@@ -6,20 +7,20 @@ import struct Foundation.UUID
 /// afterwards with `@unchecked Sendable`.
 final class Todo: Model, @unchecked Sendable {
     static let schema = "todos"
-    
+
     @ID(key: .id)
     var id: UUID?
 
     @Field(key: "title")
     var title: String
 
-    init() { }
+    init() {}
 
     init(id: UUID? = nil, title: String) {
         self.id = id
         self.title = title
     }
-    
+
     func toDTO() -> TodoDTO {
         .init(
             id: self.id,
